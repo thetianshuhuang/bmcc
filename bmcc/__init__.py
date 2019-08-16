0d3d069d7ef717e2ed13785bba0e969da13b3ac1
Tianshu Huang <thetianshuhuang@gmail.com>
2019-08-16 15:38:56 -0500

    Updated references in main module docstring

--- bmcc/__init__.py
+++ bmcc/__init__.py
@@ -13,36 +13,40 @@ Tianshu Huang
 
 Summary
 -------
-Implementation of Markov Chain Bayesian Clustering techniques, including DPM
-(Dirichlet Process Mixture Models [1]) and MFM (Mixture of Finite Mixtures [2])
-mixture models, with an abstract Mixture Model and Component Model API.
+Implementation of Markov Chain Monte Carlo Bayesian Clustering techniques,
+including DPM (Dirichlet Process Mixture Models; Neal, 2000) and MFM (Mixture
+of Finite Mixtures; Miller & Harrison, 2018)) mixture models, with an abstrac
+ Mixture Model and Component Model API.
 
-Hyperparameter updates for DPM are implemented using an Empirical Bayes update
-procedure [3].
+Both Gibbs samplers Neal, 2000, Split Merge samplers Jain & Neal, 2012 are
+implemented.
+
+Hyperparameter updates for DPM are (optionally) implemented using an Empirical
+Bayes update procedure (McAuliffe et. al., 2006).
 
 Final configuration selection is implemented using Least Squares clustering
-[4].
+(Dahl, 2006).
 
 For more information, see the Github repository:
 https://github.com/thetianshuhuang/bmcc
 
 References
 ----------
-[1] Radford M. Neal (2000), "Markov Chain Sampling Methods for Dirichlet
-    Process Mixture Models". Journal of Computational and Graphical Statistics,
-    Vol. 9, No. 2.
-
-[2] Jeffrey W. Miller, Matthew T. Harrison (2018),
-    "Mixture Models with a Prior on the Number of Components".
-    Journal of the American Statistical Association, Vol. 113, Issue 521.
-
-[3] Jon D. McAuliffe, David M. Blei, Michael I. Jordan (2006),
-    "Nonparametric empirical Bayes for the Dirichlet process mixture model".
-    Statistics and Computing, Vol. 16, Issue 1.
-
-[4] David B. Dahl (2006), "Model-Based Clustering for Expression Data via a
+David B. Dahl (2006), "Model-Based Clustering for Expression Data via a
     Dirichlet Process Mixture Model". Bayesian Inference for Gene Expression
     and Proteomics.
+Sonia Jain, Radford M. Neal (2004), "A Split-Merge Markov Chain Monte Carlo
+    Procedure for the Dirichlet Process Mixture Model". Journal of
+    Computational and Graphical Statistics, Vol 13, Issue 1.
+Jon D. McAuliffe, David M. Blei, Michael I. Jordan (2006), "Nonparametric
+    empirical Bayes for the Dirichlet process mixture model". Statistics and
+    Computing, Vol. 16, Issue 1.
+Jeffrey W. Miller, Matthew T. Harrison (2018), "Mixture Models with a Prior on
+    the Number of Components". Journal of the American Statistical Association,
+    Vol. 113, Issue 521.
+Radford M. Neal (2000), "Markov Chain Sampling Methods for Dirichlet Process
+    Mixture Models". Journal of Computational and Graphical Statistics, Vol. 9,
+    No. 2.
 """
 
 
